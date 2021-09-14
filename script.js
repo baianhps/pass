cd7c1f4c0c56b0e2e3922fc250a262f8fe313196
baianhps <baianhps@gmail.com>
2021-09-14 17:10:38 +0800

    3

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let names = ['張彥君', '劉愛育', '林君利', '林宛齊', '黃春宏', '周子瑜']
 
-window.onload = function () {
-    let n=names[Math.floor(Math.random()*names.length)]
-    document.getElementById('custum-name').innerText=n
+window.onload = function() {
+    document.getElementById('custum-name').innerText = names[Math.floor(Math.random() * names.length)]
     document.getElementById('custum-date').innerText = date()
     document.getElementById('custum-color').classList.add('backgcolor_' + day())
-    console.log(n)
 }
+
 function date() {
     let d = new Date()
     let month = d.getMonth() + 1
@@ -15,7 +14,7 @@ function date() {
     if (date < 10) { date = '0' + date }
     return month + '/' + date
 }
+
 function day() {
     return new Date().getDay()
 }
-
